3a3c4167be03def241f5196f18587c51484825ef
Met Dekaj <met.dekaj@netcompany.com>
2021-04-26 16:07:27 +0100

    Testing; returning 4 deals

--- src/scripts/__tests__/Store.js
+++ src/scripts/__tests__/Store.js
@@ -2,15 +2,23 @@ import Store from "../Store";
 import mockData from "../../../public/db.json";
 
 describe("filter", () => {
-  it("should return all deals when no filters applied", () => {
-    // Arrange
-    const sut = new Store();
+  let sut;
+  beforeEach(() => {
+    sut = new Store();
     sut.setDeals(mockData.deals);
+  });
 
+  it("should return all deals when no filters applied", () => {
     // Act
     const result = sut.deals;
 
     // Assert
     expect(result).toEqual(mockData.deals);
+  });
+
+  it("should return 4 deals when productFilter = broadband", () => {
+    sut.setProductFilter("broadband");
+    const result = sut.deals;
+    expect(result.length).toEqual(4);
   });
 });
